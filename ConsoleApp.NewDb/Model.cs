7ccdc53ddde8a9ff79b48865a95d09c13d6036af
Faizan Hussain <faizan.hussain@tenpearls.com>
2018-09-27 17:42:38 +0500

    Initial commit

---
+++ ConsoleApp.NewDb/Model.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace ConsoleApp.NewDb
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Employee>()
+                .Property(e => e.Designation)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (Desgination)Enum.Parse(typeof(Desgination), v));
+        }
+    }
+
+    public class Employee
+    {
+        #region Simple Constructor
+        public Employee(string name, Desgination designation)
+        {
+            Name = name;
+            Designation = designation;
+        }
+        #endregion
+
+        #region Simple Model properties
+        [Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+
+        public Desgination Designation { get; set; }
+        
+        #endregion
+
+        #region Constructor with DbContext & Lazy loading
+        private EmployeeContext Context { get; set; }
+        private ILazyLoader LazyLoader { get; set; }
+        private Employee(EmployeeContext context, ILazyLoader lazyLoader)
+        {
+            Context = context;
+            LazyLoader = lazyLoader;
+        }
+        #endregion
+
+        #region DbContext utilization
+        public int DepartmentCount
+            => Departments?.Count
+               ?? Context?.Set<Department>().Count(d => d.Id == EF.Property<int?>(d, "EmployeeEmpId"))
+               ?? 0;
+        #endregion
+
+        #region Lazy loading utilization
+        private ICollection<Department> _departments;
+        public ICollection<Department> Departments
+        {
+            get => LazyLoader.Load(this, ref _departments);
+            set => _departments = value;
+        }
+        #endregion
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public Employee Employee { get; set; }
+    }
+
+    public enum Desgination
+    {
+        CEO,
+        Manager,
+        Superviser,
+        Engineer,
+        Intern
+    }
+}
---
+++ ConsoleApp.NewDb/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp.NewDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EmployeeContext())
+            {
+                var emp1 = new Employee("New Emp", Desgination.Engineer)
+                {
+                    Departments = new List<Department>
+                    {
+                        new Department
+                        {
+                            DepartmentName = "HR"
+                        },
+                        new Department
+                        {
+                            DepartmentName = "Management"
+                        }
+                    }
+                };
+                db.Employees.Add(emp1);
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+                Console.WriteLine();
+                Console.WriteLine("All employees in database:");
+                foreach (var employee in db.Employees)
+                {
+                    Console.WriteLine("Employee Id - {0}, Employee Name - {1}, No. of Depts works in - {2}", employee.EmpId, employee.Name, employee.DepartmentCount);
+
+                    foreach (var dept in employee.Departments)
+                    {
+                        Console.WriteLine("Employee Id - {0} belongs to this Department - {1}", employee.EmpId,
+                            dept.DepartmentName);
+                    }
+                }
+                
+            }
+        }
+    }
+}
